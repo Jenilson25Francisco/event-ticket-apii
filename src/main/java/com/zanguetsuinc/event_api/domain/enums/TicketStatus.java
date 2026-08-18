f0109be8550d28b1a36760a8ab13b2d0baa15149
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-08-18 14:08:16 +0100

    feat: add enums for event, QR code, ticket, and validation statuses

---
+++ src/main/java/com/zanguetsuinc/event_api/domain/enums/TicketStatus.java
@@ -0,0 +1,7 @@
+package com.zanguetsuinc.event_api.domain.enums;
+
+public enum TicketStatus {
+
+    PURCHASED, CANCELLED
+
+}
---
+++ src/main/java/com/zanguetsuinc/event_api/domain/enums/TicketvalidationMethod.java
@@ -0,0 +1,7 @@
+package com.zanguetsuinc.event_api.domain.enums;
+
+public enum TicketvalidationMethod {
+
+    QR_CODE, MANUAL
+
+}
